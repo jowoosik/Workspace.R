fe625d241054465dcbeab017ab1b1d612f4ce4bc
jowoosik <65219895+jowoosik@users.noreply.github.com>
2020-05-18 17:55:49 +0900

    05일차
    데이터 복습하기

--- 05.R
+++ 05.R
@@ -16,10 +16,17 @@
 #
 # Text file 읽기
 setwd( "C:\\WorkspaceR" ) # 파일 저장 경로 설정
-df <- read.table( file = "airquality.txt", header = T )
-df
+df.txt <- read.table( file = "airquality.txt",
+                      header = T)
 
-class ( df )
+df.txt
+
+class ( df.txt )
+str( df.txt)
+head( df.txt)
+tail( df.txt)
+
+
 
 #
 # Excel 파일 읽기
@@ -61,6 +68,257 @@ score
 
 idx <- which(df.xlsx[ , 1:2 ] == "NA", arr.ind = TRUE )  # arr.ind : 몇행 몇열.
 idx
+
+
+# 2.2 자료의 종류
+#
+# - 자료의 종류에 따라 적용할수 있는 분석 방법이 다르다.
+# - 분석을 해야할 자료를 가지고 있을 때 1차적으로 해야 할 일은
+#   해당 자료가 어떤 분류에 속하는지를 파악하는 일이다.
+#
+# - 자료 특성에 따른 분류
+#   분석 대상 자료의 특성에 따라 범주형 자료와 연속형 자료로 분류한다
+#
+# 1. 범주형 자료 ( categorical data) : 분류하고 그룹화.
+#    - 범주 또는 그룹으로 구분할 수 있는 값
+#    - 범주형 자료의 값은 기본적으로 숫자로 표현할 수 없고,
+#      대소 비교나 산순연산이 적용되지 않는다.
+#    - 범주형 자료는 대체로 문자형 값을 갖는데, 숫자로 표기할수 있으나
+#      계산이 가능한 연속형 자료가 되는 것은 아니다.
+#
+# 2. 연속형 자료 ( numnerical data)
+#    - 크기가 있는 숫자들로 구성된 자료료
+#    - 연속형 자료의 값들은 대소비교, 산술연산이 가능하기 때문에 다양한
+#      분석 방법이 존재한다.
+#
+#
+#
+# 자료 ( Data ) : 어떤 주제를 가지고 값을 모아 놓은 것 전체
+# 관측값( observation ) : 자료에 포홤된 것들
+#
+# 통계학에서의 변수는 "연구, 조사, 관찰하고 싶은 대상의 특성(feature) "
+#
+# 변수 개수에 따라
+#   1. 단일변수 자료( univariate data ) , 일변량 자료
+#      하나의 변수로 구성된 자료
+#   2. 다중변수 자료( multivariate data) , 다변량자료
+#      두 개 이상의 변수로 구성된 자료
+#      특별히 두 개의 변수로 구성된 자료를 이변량 자료( bivaritate data )
+#
+#   단일 변수 자료는 vector에 저장하여 분석
+#   다중 변수 자료는 matrix 또는 data frame에 저장하여 분석
+#
+#  변수의 개수와 자료의 특성에 따른 분류
+# 1. 단일변수 범주형 자료
+# 2. 단일변수 연속형 자료
+# 3. 다중변수 범주형 자료
+# 4. 다중변수 연속형 자료
+#
+# 2.3 단일 변수 범주형 자료 탐색
+#  ( 범주형 : 혈액형, 품종, 계절, 성별 etc, 범주형은 산술연산 x , table(빈도수, 함수 쓰기)
+# 단일 변수 범주형 자료 : 특성이 하나이면서 자료의 특성이 범주형인 자료
+#   범주형 자료에 할 수 있는 기본 작업 : 자료에 포함된 관측값들의 
+#                          도수분포표 <- 종류별로 개수를 세는 것,                                      종류별로 비율을 구하는 것,
+#                                        종류별로 비율을 구하는 것,
+#                                        시각화는 막대/원 그래프를 사용
+
+#
+#
+# 변수가 하나 -> 벡터(c) -> 특성(변수)
+favorite <- c( 'WINTER', 'SUMMER', 'SPRING',
+               'SUMMER', 'SUMMER', 'FALL',
+               'FALL', 'SUMMER', 'SPRING', 'SPRING')
+favorite
+
+class( favorite) # type
+str( favorite)   # 내부구조
+dim( favorite)   # data 수
+
+# 도수분포표 작성
+table( favorite)
+
+# 도수 분포표 비율
+table( favorite ) / length( favorite)
+
+# 시각화( 막대 그래프)
+ds <- table( favorite)
+ds
+
+class( ds )
+str( ds) 
+dim ( ds) 
+
+barplot( ds, main = 'favorite season') # 기본 막대그래프를 만들어주는 함수, ( ds, mian = 막대이름)
+
+ds.new <- ds[ c(2, 3, 1, 4) ]
+ds.new
+barplot( ds.new, main = 'favorite season')  # 면 적  
+
+pie( ds, main = 'favorite season')          # 전체적인 비율
+pie ( ds.new , main = 'favorite season')    # 
+
+
+favorite.color <- c( 2, 3, 2, 1, 1, 2, 2,
+                     1, 3, 2, 1, 3, 2, 1, 2 )
+ds <- table( favorite.color )
+ds
+# 위와 같이 범주형은 일단 도수분포표, 비율
+
+barplot( ds, main = 'favorite season' )
+
+
+colors <- c( 'green' , 'red', 'blue')
+names( ds ) <- colors;
+ds
+
+barplot( ds, main = 'favorite season',            # 범주형 도수분포표
+         col = colors)
+
+pie( ds, main = 'favorite season',
+         col = colors)
+# 도수 분포표
+# y : 도수, x : 범주형 데이터 
+
+#
+# 2.4 단일 변수 연속형 자료 탐색
+#
+# 단일 변수 연속형 자료 : 특성이 하나이면서 자료의 특성이 연속형인 자료
+#       연속형 자료는 관측값들이 크기를 가지므로 다양한 분석방법 존재
+#
+# 1. 평균( mean ) : 하나의 값으로 전체를 대표할 수 있는 값,
+#                   이상값에 영향을 받는다.
+# 2. 중앙값( median ) : 자료의 값들을 크기순으로 정렬하였을때
+#                       가장 중앙에 위치한 값,
+#                       이상값에 영향을 받지 않는다. (홀수 일떄 1개, 짝수는 2개)
+# 3. 절사평균 ( tirmmed mead) : 자료의 관측값들 중에서 작은 값들의 하위
+#                               n%와 큰 값의 상위 n%를 제외하고 중간에있는
+#                               나머지값들만 가지고 평균을 계산하는 방식 / 시험점수, 
+# 4. 사분위수( quatrile) : 주어진 자료에 있는 값들을 크기순으로 나열
+#                          했을 때 4등분하는 지점에 있는 값
+#           1사분위수(Q1), 2사분위수(Q2, 중앙값과 동일) , 3사분위수(Q3)
+#           전체 자료를 4개로 나누므로 4개 구간은 25%의 자료가 존재   / 범위밖은 이상치( 문제에 따라 절삭)
+# 5. 산포( distribution) : 주어진 자료에 있는 값들이 퍼져 있는 정도
+#   분산( variance ) : 주어진 자료의 각각의 값이 평균으로부터 떨어져
+#                      있는 정도를 계산하여 합산한 후 값들의 개수로
+#                      나누어 계산
+#   표준편차( standard deviation) : 분산의 제곱근으로 계산
+#       
+#   시각화는 히스토그램(Histogram)과 상자 그래프(box plot)를 사용.
+#  
+weight <- c( 60, 62, 64, 65, 68, 69)
+weight
+
+weight.heavy <-  c(  weight, 120)
+weight.heavy
+
+# 평균
+mean ( weight )
+mean ( weight.heavy)
+
+# 중앙값
+median ( weight)
+median ( weight.heavy)
+
+#x / 2, x / 2 +1
+
+# 절사평균 ( x , trim = n% )
+mean ( weight, trim = 0.2 )
+mean ( weight.heavy, trim = 0.2)
+
+# -----> 평균값과 중앙값,절사평균값의 차이가 있을 때 평균값을 사용하기 어렵다.
+
+# 사분위수
+quantile( weight.heavy )
+quantile( weight.heavy, ( 0:10) / 10 )
+summary( weight.heavy)
+
+# 산포
+# 분산
+var( weight)
+
+#표준편차
+sd( weight )
+
+# 값의 범위(최소값과 최대값)
+range ( weight)
+
+# 최대값과 최소값의 차이
+diff ( range ( weight ) )
+
+# 연속형 자료 시각화 
+# Histogram : 연속형 자료의 분포를 시각화 하는 도구
+#             연속형 자료에서의 구간을 나누고 구간에 속한
+#             값들의 개수를 세는 방법으로 사용
+
+class( cars )
+str ( cars )
+
+dist <- cars[ , 2 ]
+dist 
+
+#그래프와 값을 비교해볼 수 있음.
+boxplot.stats( dist ) # 구체적인 값을 파악 할 때 사용
+                      # $stats : 사분위수
+                      # $n : 관측값의 수
+                      # $conf : 중앙값에 대한 신뢰구간 
+                      # $out : 특이값( 이상치 ) 목록
+
+hist( dist, main = "Histogram for 제동거리",         # 연속형 도수 분포 / 구간의 빈도수를 구분할 때.
+      xlab = "제동거리", ylab = "빈도수",            # 연속형 , 범주형 = 둘 다 빈도 수를 구함 but, 연속형은 구간, 범주형은넓이 
+      border = 'blue', col = 'green',
+      las = 2, breaks = 5)
+ # 본질을 먼저 한 뒤, 나중에 그래프로 표현.
+
+
+# 상자 그림( boxplot, 상자 수염 그래프)
+#    사분위수를 그래프형태로 시각화하는 도구
+#    상자그래프는 하나의 그래프로 데이이터의 분포 
+#    형태를 포함한 다양한 정보를 전달 
+#    - 자료의 전반적인 분포를 이해하는데 도움
+#    - 구체적인 최소/최대/중앙값을 알기는 어렵다. / 구체적인 값을 알 때 : boxplot.stats or summary 함수 
+
+boxplot( dist, main = '자동차 제동거리' ,
+         col = 'black' , border = 'red') 
+
+# 빅데이터는 이미 발생된 데이터를 이면을 분석하고 숨겨진 의미를 이해하여 우리 삶을 윤택하게 한다. 
+
+boxplot.stats( dist )
+boxplot.stats( dist )$stats # 정상범위 사분위수
+boxplot.stats( dist )$n     # 관측치 수
+boxplot.stats( dist )$conf  # 중앙값 신뢰구간
+boxplot.stats( dist )$out   # 특이값 ( 이상치 ) 목록
+
+# 일변량 연속형 데이터 중 그룹으로 구성된 
+# 자료의 상자그래프            ( boxplot ( x ~ 그룹별 , data, --- ) 
+boxplot( Petal.Length~Species, data = iris,
+         main = "품종별 꽃잎 길이",
+         border = "red", col = "black")
+
+
+str ( iris )
+
+boxplot( iris$Petal.Length~iris$Species,
+         main = "품종별 꽃잎 길이",
+         border = "red", col = "black")    # ( ~, 그룹정보가 있을 때 )
+
+
+# 한 화면에 여러 그래프 생성
+par( mfrow = c( 1, 3))      # 1 X 3 가상화면 분할
+barplot( table ( mtcars$carb), main = "C",
+         xlab = ' carburetors', ylab = ' freq',
+         col = 'blue', border = 'red')
+barplot( table( mtcars$cyl ), main = 'Cyl',
+         xlab = 'cy1', ylab = 'freq', 
+         col = 'red', border = 'blue')
+barplot ( table( mtcars$gear), main = 'g', 
+          xlab = 'gear' , ylab = 'freq',
+          col = 'green', border = 'black')
+
+str( mtcars)
+
+
+par( mfrow = c( 1, 1) ) # 가상화면 분할 해제
+
 
 
 
